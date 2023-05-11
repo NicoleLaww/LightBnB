6a6d178685260cd29e28520f5802e6e32ea75e25
Nicole Law <nicole.law12@gmail.com>
2023-05-11 19:09:06 -0400

    queries completed

---
+++ 1_queries/3_property_listings_by_city.sql
@@ -0,0 +1,9 @@
+SELECT properties.id, title, cost_per_night, AVG(property_reviews.rating) AS average_rating 
+FROM properties 
+JOIN property_reviews ON properties.id = property_id
+WHERE city LIKE '%ancouver'
+GROUP BY properties.id, title, cost_per_night
+HAVING AVG(property_reviews.rating) >= 4
+ORDER BY cost_per_night ASC 
+LIMIT 10;
+
