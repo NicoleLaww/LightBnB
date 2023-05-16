6c8b57fcd3af51548699125439ca9e2b79443dc7
Nicole Law <nicole.law12@gmail.com>
2023-05-16 16:03:30 -0400

    lighthousebnb properties

--- 1_queries/3_property_listings_by_city.sql
+++ 1_queries/3_property_listings_by_city.sql
@@ -1,9 +1,9 @@
-SELECT properties.id, title, cost_per_night, AVG(property_reviews.rating) AS average_rating 
-FROM properties 
-JOIN property_reviews ON properties.id = property_id
-WHERE city LIKE '%ancouver'
-GROUP BY properties.id, title, cost_per_night
-HAVING AVG(property_reviews.rating) >= 4
-ORDER BY cost_per_night ASC 
+SELECT properties.id, title, cost_per_night, avg(property_reviews.rating) as average_rating
+FROM properties
+LEFT JOIN property_reviews ON properties.id = property_id
+WHERE city LIKE '%ancouv%'
+GROUP BY properties.id
+HAVING avg(property_reviews.rating) >= 4
+ORDER BY cost_per_night
 LIMIT 10;
 
--- 1_queries/4_most_visited_cities.sql
+++ 1_queries/4_most_visited_cities.sql
@@ -1,5 +1,5 @@
-SELECT city, COUNT(reservations.*) AS total_reservations
-FROM properties 
-JOIN reservations ON properties.id = property_id 
-GROUP BY city 
+SELECT properties.city, count(reservations) as total_reservations
+FROM reservations
+JOIN properties ON property_id = properties.id
+GROUP BY properties.city
 ORDER BY total_reservations DESC;
