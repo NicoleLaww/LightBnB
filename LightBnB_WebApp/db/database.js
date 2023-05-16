d0a9aecae93278fdf48b79060cd2e60e2e8a2299
Nicole Law <nicole.law12@gmail.com>
2023-05-15 23:11:03 -0400

    reservations

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -110,8 +110,27 @@ const addUser = function(user) {
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
-const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+// const getAllReservations = function(guest_id, limit = 10) {
+//   return getAllProperties(null, 2);
+// };
+
+const getAllReservations = (guest_id, limit = 10) => {
+  return pool
+    .query(`SELECT properties.*, reservations.id, reservations.start_date, avg(rating) as average_rating
+      FROM reservations
+      JOIN properties ON reservations.property_id = properties.id
+      JOIN property_reviews ON properties.id = property_reviews.property_id
+      WHERE reservations.guest_id = $1
+      GROUP BY properties.id, reservations.id
+      ORDER BY reservations.start_date
+      LIMIT $2`, [guest_id, limit])
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((err) => {
+      // console.log(err.message);
+      throw err;
+    });
 };
 
 /// Properties
@@ -130,24 +149,15 @@ const getAllReservations = function(guest_id, limit = 10) {
 //   return Promise.resolve(limitedProperties);
 // };
 
-const getAllProperties = (guestId, limit = 10) => {
+const getAllProperties = (options, limit = 10) => {
   return pool
-    .query(`SELECT properties.*, reservations.id, reservations.start_date, avg(rating) as average_rating
-      FROM reservations
-      JOIN properties ON reservations.property_id = properties.id
-      JOIN property_reviews ON properties.id = property_reviews.property_id
-      WHERE reservations.guest_id = $1
-      GROUP BY properties.id, reservations.id
-      ORDER BY reservations.start_date
-      LIMIT $2`, [2, limit])
+    .query(`SELECT * FROM properties LIMIT $1`, [limit])
     .then((result) => {
-      console.log(guestId);
-      // console.log(result.rows[0]);
-      return result.rows[0];
+      console.log(result.rows);
+      return result.rows;
     })
     .catch((err) => {
-      // console.log(err.message);
-      throw err;
+      console.log(err.message);
     });
 };
 
