2f6ab089f2e0259d8f2b3064eed3b67d18d5e193
Nicole Law <nicole.law12@gmail.com>
2023-05-20 17:39:23 -0400

    updated readme/images

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -21,16 +21,6 @@ const users = require("./json/users.json");
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-// const getUserWithEmail = function (email) {
-//   let resolvedUser = null;
-//   for (const userId in users) {
-//     const user = users[userId];
-//     if (user?.email.toLowerCase() === email?.toLowerCase()) {
-//       resolvedUser = user;
-//     }
-//   }
-//   return Promise.resolve(resolvedUser);
-// };
 
 const getUserWithEmail = function(email) {
   return pool
@@ -56,9 +46,6 @@ const getUserWithEmail = function(email) {
  * @param {string} id The id of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-// const getUserWithId = function(id) {
-//   return Promise.resolve(users[id]);
-// };
 
 const getUserWithId = function(id) {
   return pool
@@ -83,12 +70,6 @@ const getUserWithId = function(id) {
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
-// const addUser = function(user) {
-//   const userId = Object.keys(users).length + 1;
-//   user.id = userId;
-//   users[userId] = user;
-//   return Promise.resolve(user);
-// };
 
 const addUser = function(user) {
   return pool
@@ -110,9 +91,6 @@ const addUser = function(user) {
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
-// const getAllReservations = function(guest_id, limit = 10) {
-//   return getAllProperties(null, 2);
-// };
 
 const getAllReservations = (guest_id, limit = 10) => {
   return pool
@@ -141,25 +119,6 @@ const getAllReservations = (guest_id, limit = 10) => {
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-// const getAllProperties = function (options, limit = 10) {
-//   const limitedProperties = {};
-//   for (let i = 1; i <= limit; i++) {
-//     limitedProperties[i] = properties[i];
-//   }
-//   return Promise.resolve(limitedProperties);
-// };
-
-// const getAllProperties = (options, limit = 10) => {
-//   return pool
-//     .query(`SELECT * FROM properties LIMIT $1`, [limit])
-//     .then((result) => {
-//       console.log(result.rows);
-//       return result.rows;
-//     })
-//     .catch((err) => {
-//       console.log(err.message);
-//     });
-// };
 
 const getAllProperties = function(options, limit = 10) {
 
@@ -242,12 +201,6 @@ const getAllProperties = function(options, limit = 10) {
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
-// const addProperty = function(property) {
-//   const propertyId = Object.keys(properties).length + 1;
-//   property.id = propertyId;
-//   properties[propertyId] = property;
-//   return Promise.resolve(property);
-// };
 
 const addProperty = function(property) {
   const queryParams = [];
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # LightBnB
+* A simple multi-page Airbnb clone that uses a server-side Javascript to display the information from queries to web pages via SQL queries.
+
+# Based on this Entity Relationship Diagram...
+!["ERD"]()
+
+# Dependencies
+- bcryptjs
+- cookie-session
+- Express
+- nodemon
+- pg
+
+# Getting Started
+- Install all dependencies (using the `npm install` command)
+- From the LightBnB_WebApp-master directory, run `npm run local`
+- Go to  http://localhost:3000/ in your browser to view the app
